25cead6720136c35a49d8a7d72c8f6c3321db98f
TomasUhiaOtero <tomasuhiaotero@gmail.com>
2025-05-04 14:46:36 +0000

    Proyecto JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let random4 = Math.floor(Math.random*4);
+  let random3 = Math.floor(Math.random*3);
+  let random5 = Math.floor(Math.random*5);
+  console.log(who[random4]+action[random4]+what[random3]+when[random5]);
+  document.getElementById("excuse").innerHTML=who[random4]+action[random4]+what[random3],when[random5];
+
 };
